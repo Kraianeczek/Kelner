07e891ec20ac4b291786d2e72a5619bc84fd477e
Tomasz Krajewski <krajaneczek@gmail.com>
2022-02-16 20:46:24 +0100

    wadd singleTable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
+import Dropdown from "./components/common/dropdown/dropdown";
 import Main from "./components/pages/main/main";
 import MissingPage from "./components/pages/missingpage/missingPage";
 import Footer from "./components/views/footer/footer";
 import Header from "./components/views/header/header";
+import SingleTable from "./components/views/singleTable/singleTable";
 import Tables from "./components/views/tables/tables";
 import { fetchTables } from "./redux/tablesRedux";
 
@@ -22,6 +24,8 @@ function App() {
           <Route path="*" element={<MissingPage />} />
         </Routes>
       <Footer />
+      <SingleTable />
+      <Dropdown />
     </div>
   );
 }
